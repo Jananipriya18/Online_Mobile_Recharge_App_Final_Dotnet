b7d972b72809fe6aecd05f4aeca7146c9ed2a837
Iamneo <techadmin@iamneo.ai>
2024-01-03 14:02:39 +0000

    Auto-save-commit-1/3/2024--7:32:38-PM

--- dotnetapp/Program.cs
+++ dotnetapp/Program.cs
@@ -1,16 +1,8 @@
-using dotnetapp.Models;
-using Microsoft.EntityFrameworkCore;
-using dotnetapp.Data;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString")));
-    
-    
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
