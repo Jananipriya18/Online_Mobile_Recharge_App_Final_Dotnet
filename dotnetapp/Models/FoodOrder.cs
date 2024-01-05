f61be06566601fc5f976d4d9fdbc7db87cec7bf1
neojarvis <techadmin@iamneo.ai>
2024-01-05 06:54:26 +0000

    Auto-save-commit-1/5/2024--12:24:23-PM

--- dotnetapp/Models/FoodOrder.cs
+++ dotnetapp/Models/FoodOrder.cs
@@ -9,7 +9,6 @@ namespace dotnetapp.Models
     {
         public int Id { get; set; }
         public DateTime Date { get; set; }
-        public int OrderID { get; set; }
         public string FoodName { get; set; }
         public decimal Price {get; set;}
         public int rating { get; set;}
