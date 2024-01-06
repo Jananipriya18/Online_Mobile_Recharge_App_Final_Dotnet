0dbf6f044fa790210a3f650eb37e7ddd9fc69787
neojarvis <techadmin@iamneo.ai>
2024-01-06 10:06:08 +0000

    Auto-save-commit-1/6/2024--3:36:07-PM

--- SimCardComplaint/dotnetapp/Controllers/ComplaintController.cs
+++ SimCardComplaint/dotnetapp/Controllers/ComplaintController.cs
@@ -16,47 +16,69 @@ namespace dotnetapp.Controllers
             _db = db;
         }
 
-        public ActionResult Create()
-        {
-            var executives = _db.Executives
-                .Select(e => new SelectListItem
-                {
-                    Value = e.ExecutiveID.ToString(),
-                    Text = e.ExecutiveName
-                })
-                .ToList();
+        // public ActionResult Create()
+        // {
+        //     var executives = _db.Executives
+        //         .Select(e => new SelectListItem
+        //         {
+        //             Value = e.ExecutiveID.ToString(),
+        //             Text = e.ExecutiveName
+        //         })
+        //         .ToList();
 
-            // Always pass the executives to the view, whether it's empty or not
-            ViewBag.Executives = executives;
+        //     // Always pass the executives to the view, whether it's empty or not
+        //     ViewBag.Executives = executives;
 
-            return View();
-        }
+        //     return View();
+        // }
 
+        // [HttpPost]
+        // [ValidateAntiForgeryToken]
+        // public ActionResult Create(Complaint newComplaint)
+        // {
+        //     if (ModelState.IsValid)
+        //     {
+        //         // Fetch the selected executive from the database based on the provided ExecutiveID
+        //         var selectedExecutive = _db.Executives.FirstOrDefault(e => e.ExecutiveID == newComplaint.ExecutiveID);
+
+        //         if (selectedExecutive != null)
+        //         {
+        //             // Associate the selected executive with the new complaint
+        //             newComplaint.Executive = selectedExecutive;
+        //             // Add the complaint to the database
+        //             _db.Complaints.Add(newComplaint);
+        //             _db.SaveChanges();
+        //             return RedirectToAction("Dashboard");
+        //         }
+        //         else
+        //         {
+        //             ModelState.AddModelError("", "Selected Executive not found");
+        //         }
+        //     }
+
+        //     // If the model state is not valid or the executive wasn't found, retrieve executives again and return to the view
+        //     var executives = _db.Executives
+        //         .Select(e => new SelectListItem
+        //         {
+        //             Value = e.ExecutiveID.ToString(),
+        //             Text = e.ExecutiveName
+        //         })
+        //         .ToList();
+
+        //     ViewBag.Executives = executives;
+        //     return View(newComplaint);
+        // }
+        
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(Complaint newComplaint)
+[ValidateAntiForgeryToken]
+public ActionResult Create(Complaint newComplaint)
+{
+    if (ModelState.IsValid)
+    {
+        // Check if the ExecutiveID is provided
+        if (newComplaint.ExecutiveID == 0) // Assuming 0 is the default value for ExecutiveID
         {
-            if (ModelState.IsValid)
-            {
-                // Fetch the selected executive from the database based on the provided ExecutiveID
-                var selectedExecutive = _db.Executives.FirstOrDefault(e => e.ExecutiveID == newComplaint.ExecutiveID);
-
-                if (selectedExecutive != null)
-                {
-                    // Associate the selected executive with the new complaint
-                    newComplaint.Executive = selectedExecutive;
-                    // Add the complaint to the database
-                    _db.Complaints.Add(newComplaint);
-                    _db.SaveChanges();
-                    return RedirectToAction("Dashboard");
-                }
-                else
-                {
-                    ModelState.AddModelError("", "Selected Executive not found");
-                }
-            }
-
-            // If the model state is not valid or the executive wasn't found, retrieve executives again and return to the view
+            ModelState.AddModelError("ExecutiveID", "Executive ID is required");
             var executives = _db.Executives
                 .Select(e => new SelectListItem
                 {
@@ -68,6 +90,37 @@ namespace dotnetapp.Controllers
             ViewBag.Executives = executives;
             return View(newComplaint);
         }
+
+        // Fetch the selected executive from the database based on the provided ExecutiveID
+        var selectedExecutive = _db.Executives.FirstOrDefault(e => e.ExecutiveID == newComplaint.ExecutiveID);
+
+        if (selectedExecutive != null)
+        {
+            // Associate the selected executive with the new complaint
+            newComplaint.Executive = selectedExecutive;
+            // Add the complaint to the database
+            _db.Complaints.Add(newComplaint);
+            _db.SaveChanges();
+            return RedirectToAction("Dashboard");
+        }
+        else
+        {
+            ModelState.AddModelError("ExecutiveID", "Selected Executive not found");
+        }
+    }
+
+    // If the model state is not valid or the executive wasn't found, retrieve executives again and return to the view
+    var executivesList = _db.Executives
+        .Select(e => new SelectListItem
+        {
+            Value = e.ExecutiveID.ToString(),
+            Text = e.ExecutiveName
+        })
+        .ToList();
+
+    ViewBag.Executives = executivesList;
+    return View(newComplaint);
+}
 
         public ActionResult Dashboard()
         {
