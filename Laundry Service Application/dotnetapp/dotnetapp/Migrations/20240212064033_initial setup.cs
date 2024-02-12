46a45f35c82ec16c4e6a8dd6e003a0c4668ebcf7
neojarvis <techadmin@iamneo.ai>
2024-02-12 06:52:35 +0000

    Auto-save-commit-12/02/2024--12:22:35

---
+++ Laundry Service Application/dotnetapp/dotnetapp/Migrations/20240212064033_initial setup.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnetapp.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserSchedules",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MobileNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PickupDay = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PickupTimeSlot = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserSchedules", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserSchedules");
+        }
+    }
+}
--- Laundry Service Application/dotnetapp/dotnetapp/Program.cs
+++ Laundry Service Application/dotnetapp/dotnetapp/Program.cs
@@ -16,7 +16,6 @@ builder.Services.AddCors(options =>
 });
 
 
-
 // Add services to the container.
 
 builder.Services.AddControllers();
