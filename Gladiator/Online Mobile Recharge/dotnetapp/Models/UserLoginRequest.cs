b95a3cb7dcae5a8572c947f346c39ae563d2aa84
neojarvis <techadmin@iamneo.ai>
2024-02-13 10:01:40 +0000

    Auto-save-commit-13/02/2024--15:31:40

--- Gladiator/Online Mobile Recharge/dotnetapp/Models/UserLoginRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BookStoreDBFirst.Models
-{
-    public class UserLoginRequest
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-
-}
