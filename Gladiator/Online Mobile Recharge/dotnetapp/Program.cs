ece6cdc1f885b8463ca00882501c2cac3f70a7cf
neojarvis <techadmin@iamneo.ai>
2024-02-13 07:31:40 +0000

    Auto-save-commit-13/02/2024--13:01:40

--- Gladiator/Online Mobile Recharge/dotnetapp/Program.cs
+++ Gladiator/Online Mobile Recharge/dotnetapp/Program.cs
@@ -1,25 +1,84 @@
+using BookStoreDBFirst.Models;
+using BookStoreDBFirst.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<JobApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString")));
+// Configure JWT authentication
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+        .AddRoles<IdentityRole>()
+        .AddEntityFrameworkStores<JobApplicationDbContext>()
+        .AddDefaultTokenProviders();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
+builder.Services.AddScoped<IUserService, UserService>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Configure Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+    // Create roles if they don't exist
+    if (!await roleManager.RoleExistsAsync("admin"))
+    {
+        await roleManager.CreateAsync(new IdentityRole("admin"));
+    }
+
+    if (!await roleManager.RoleExistsAsync("applicant"))
+    {
+        await roleManager.CreateAsync(new IdentityRole("applicant"));
+    }
+}
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(
+        //c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "LoginAuth v1")
+        );
 }
 
 app.UseHttpsRedirection();
+app.UseRouting();
 
+app.UseCors();
+
+
+// Add authentication and authorization middleware
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+Console.WriteLine("bye");
 
 app.Run();
+
