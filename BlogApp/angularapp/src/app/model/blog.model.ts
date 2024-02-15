55452a4b306a9e31b45dc8aa1b758dc7a7c1f47f
Jananipriya18 <jananijayachandran18@gmail.com>
2024-02-15 18:04:18 +0530

    Delete BlogApp directory

--- BlogApp/angularapp/src/app/model/blog.model.ts
+++
@@ -1,9 +0,0 @@
-export class Blogmodel {
-    public BlogId: number
-    public blogSubject: string
-    public blogContent: string
-    public authorName: string
-    public blogCategory: string
-    public Postedate: Date 
-  }
-  
